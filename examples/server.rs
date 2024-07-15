d24b4883493b894df77a0e725389a89a72a53f86
Conrad Ludgate <conradludgate@gmail.com>
2024-07-15 20:33:01 +0100

    make example with cli

--- examples/server.rs
+++ examples/server.rs
@@ -1,6 +1,7 @@
 use std::net::UdpSocket;
 
-use base64ct::Encoding;
+use base64ct::{Base64, Encoding};
+use clap::Parser;
 use rustyguard::{Config, Message, Peer, Sessions};
 use x25519_dalek::{PublicKey, StaticSecret};
 
@@ -9,21 +10,56 @@ use x25519_dalek::{PublicKey, StaticSecret};
 #[repr(align(16))]
 struct AlignedPacket([u8; 2048]);
 
+#[derive(clap::Parser)]
+struct Args {
+    #[arg(long)]
+    peer: Vec<String>,
+
+    #[arg(long, short)]
+    port: u16,
+
+    #[arg(long, short)]
+    key: Option<String>,
+}
+
 fn main() {
-    let private_key = StaticSecret::random();
-    println!(
-        "public key: {}",
-        base64ct::Base64::encode_string(PublicKey::from(&private_key).as_bytes())
-    );
+    let args = Args::parse();
 
-    let mut peer_pk = [0; 32];
-    base64ct::Base64::decode("5stzRvSwl0jxBteouplC3fSnwhKP+pHiaqPJ2OxgjGw=", &mut peer_pk).unwrap();
-    let peer_pk = PublicKey::from(peer_pk);
+    let private_key = match args.key {
+        Some(key) => {
+            let pk = Base64::decode_vec(&key).unwrap();
+            let private_key = StaticSecret::from(<[u8; 32]>::try_from(pk).unwrap());
+            println!(
+                "public key: {}",
+                Base64::encode_string(PublicKey::from(&private_key).as_bytes())
+            );
+            private_key
+        }
+        None => {
+            let private_key = StaticSecret::random();
+            println!(
+                "private key: {}",
+                Base64::encode_string(private_key.as_bytes())
+            );
+            println!(
+                "public key: {}",
+                Base64::encode_string(PublicKey::from(&private_key).as_bytes())
+            );
+            private_key
+        }
+    };
 
-    let config = Config::new(private_key, [Peer::new(peer_pk, None)]);
+    let mut peers = vec![];
+    for peer in args.peer {
+        let pk = Base64::decode_vec(&peer).unwrap();
+        let peer_pk = PublicKey::from(<[u8; 32]>::try_from(pk).unwrap());
+        peers.push(Peer::new(peer_pk, None));
+    }
+
+    let config = Config::new(private_key, peers);
     let mut sessions = Sessions::new(config);
 
-    let endpoint = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let endpoint = UdpSocket::bind(("0.0.0.0", args.port)).unwrap();
     println!("addr: {:?}", endpoint.local_addr());
 
     let mut buf: Box<AlignedPacket> = Box::new(AlignedPacket([0; 2048]));
