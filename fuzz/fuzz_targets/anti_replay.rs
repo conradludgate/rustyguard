65d8129dc8b867fc0e49af51047d553bc7a2fa5f
Conrad Ludgate <conradludgate@gmail.com>
2024-07-21 23:08:38 +0100

    improve fuzzer

--- fuzz/fuzz_targets/anti_replay.rs
+++ fuzz/fuzz_targets/anti_replay.rs
@@ -4,7 +4,15 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<u64>| {
     let mut replay = rustyguard::AntiReplay::default();
+
+    let mut seen = std::collections::BTreeSet::new();
+    let mut last = 0u64;
+
     for d in data {
-        replay.check(d);
+        let allowed = seen.insert(d);
+        let too_old = d < last && last - d >= 1984;
+        last = u64::max(d, last);
+
+        assert_eq!(replay.check(d), allowed && !too_old);
     }
 });
